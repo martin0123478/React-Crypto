72d207338984aecbeb17c80b2c21cec5136432bc
martin0123478 <martin_agui97@outlook.es>
2023-02-20 23:12:45 -0600

    Añadiendo estilos a los resultados

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,15 +1,56 @@
 import styled from "@emotion/styled"
 
+const Contenedor = styled.div`
+    color:#FFF;
+    font-family: 'Lato',sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+
+
+
+`
+
+const Texto = styled.p`
+font-size: 18px;
+    span{
+        font-weight: 700;
+
+    }
+
+
+`
+
+const Precio = styled.p`
+    font-size: 30px;
+    span{
+        font-weight: 700;
+
+    }
+`
+
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+
+
+`
+
 const Resultado = ({resultado}) => {
     const {PRICE,HIGHDAY,LOWDAY,CHANGEPCT24HOUR,IMAGEURL,LASTUPDATE} = resultado 
   return (
-    <div>
-      <p>El precio es de <span>{PRICE}</span></p>
-      <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-      <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-      <p>Variación ultimas 24 horas <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Ultimas actualización <span>{LASTUPDATE}</span></p>
-    </div>
+    <Contenedor>
+        <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="IMAGEN CRIPTO" />
+        <div>
+        <Precio>El precio es de <span>{PRICE}</span></Precio>
+      <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+      <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+      <Texto>Variación ultimas 24 horas <span>{CHANGEPCT24HOUR}</span></Texto>
+      <Texto>Ultimas actualización <span>{LASTUPDATE}</span></Texto>
+        </div>
+      
+    </Contenedor>
   )
 }
 
